d145d602fc681b634014f114438a559817fd65cd
Paul Bernard <paul.bernard@teleapp.com>
2014-12-27 19:49:45 -0500

    fixed typo on objc

--- bin/all-spectrum-service.sh
+++ bin/all-spectrum-service.sh
@@ -22,5 +22,5 @@ cd $APP_DIR
 ./bin/android-java-spectrum-service.sh
 ./bin/csharp-spectrum-service.sh
 ./bin/java-spectrum-service.sh
-./bin/obj-spectrum-service.sh
+./bin/objc-spectrum-service.sh
 ./bin/scala-spectrum-service.sh
